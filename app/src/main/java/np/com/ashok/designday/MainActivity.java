a1fb987dbefb59d808362a61d93139ea8601b9e5
ashokcpg <chapagaiashok@gmail.com>
2019-02-02 21:26:35 +0545

    First Commit...

---
+++ app/src/main/java/np/com/ashok/designday/MainActivity.java
@@ -0,0 +1,29 @@
+package np.com.ashok.designday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    CheckBox chkrememberme;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chkrememberme = findViewById(R.id.chkRememberMe);
+
+        chkrememberme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (chkrememberme.isChecked())
+                {
+                    Toast.makeText(MainActivity.this, "Checked!!!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+}
